6a528c655faf8d9e7305b034346a0dac2efee4eb
s-7at <43019896+s-7at@users.noreply.github.com>
2018-09-06 03:49:54 +0300

    Add files via upload

---
+++ saiif/index.php
@@ -0,0 +1,186 @@
+
+<!DOCTYPE html">
+<head>
+		<title>Saif Al-Khulaidi</title>
+		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+		<meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
+		<meta name="viewport" content="width=device-width, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+		<meta name="HandheldFriendly" content="true" />
+		<meta name="format-detection" content="telephone=no" />
+		<meta name="robots" content="index, follow" />
+		<base target="_top"/>
+		<link href="https://fonts.googleapis.com/css?family=Cairo" rel="stylesheet">
+		<link rel="stylesheet" href="css/CydiaIcon.css"/>
+		<link rel="stylesheet" href="css/style.css"/>
+		<link rel="stylesheet" href="css/menes.min.css"/>
+		<link rel="stylesheet" href="css/jssocials.css"/>
+		<link rel="stylesheet" href="css/jssocials-theme-flat.css"/>
+		<link rel="stylesheet" href="css/scroll.min.css"/>
+        <meta name="viewport" content="width=device-width, initial-scale=1"/>
+		<script src="js/fastclick.js" type="text/javascript"></script>
+		<script src="js/menes.js" type="text/javascript"></script>
+		<script src="js/cytyle.js" type="text/javascript"></script>	
+        
+</head>
+
+<style type="text/css">
+body{
+font-family: 'Cairo', sans-serif;
+    font-weight: normal;
+    font-style: normal;
+    background-image:url('img/body.png');
+    
+}
+</style>
+
+<center>
+	
+<body>
+
+<panel>
+			<fieldset>
+				<a href="cydia://url/https://cydia.saurik.com/api/share#?source=http://saiif.co/" target="_blank">
+				<img class="icon" src="img/CydiaIcon.png" />
+					<div>
+						<div>
+							<label>
+								<p>
+									 اضافة السورس  <sup><small>™</small></sup>								</p>
+							</label>
+						</div>
+					</div>
+				</a>
+			</fieldset>
+			
+			
+<br/>
+	
+<center>
+    
+    <img src="img/CydiaIcon.png" style="border-radius: 20%;border-bottom-left-radius: 20%;border-bottom-right-radius: 20%;width: 50%;">
+</center>    
+<br />
+<block style="background: #f2f2f3;color: black; ">
+	<p>
+					<strong>11</strong> عدد الادوات في السورس 
+    
+    </p>
+    
+	 <p>
+         تاريخ اخر عملية رفع تحديث او رفع :  <strong>2018-07-09 01:09:07</strong> 
+				
+     </p>
+</block>
+
+<br />
+
+			<label class="source">
+			<p> الدعم الفني </p>
+			<br/>
+            </label>
+			
+	<fieldset>
+
+				
+				<a href="http://saiif.co/" target="_blank">
+				<img class="icon" src="img/CydiaIcon.png" />
+					<div>
+						<div>
+							<label>
+								<p>الصفحة الرئيسية</p>
+							</label>
+						</div>
+					</div>
+				</a>
+				
+				
+				<a href="mailto:info@saiif.co?subject=Saif Al-Khulaidi | سيف الخليدي" target="_blank">
+				<img class="icon" src="img/email.png" />
+					<div>
+						<div>
+							<label>
+						
+								<p>Email info@saiif.co</p>
+							</label>
+						</div>
+					</div>
+				</a>
+				<a href="https://www.twitter.com/iiS4iF" target="_blank">
+				<img class="icon" src="img/twitter.png" />
+					<div>
+						<div>
+							<label>
+								<p>@iiS4iF</p>
+							</label>
+						</div>
+					</div>
+				</a>
+							<a href="https://instgaram.com/iis4if" target="_blank">
+				<img class="icon" src="img/instagram.png" />
+					<div>
+						<div>
+							<label>
+								<p>@iis4if</p>
+							</label>
+						</div>
+					</div>
+				</a>
+				<a href="https://snapchat.com/add/iis4if" target="_blank">
+				<img class="icon" src="img/snapchat.png" />
+					<div>
+						<div>
+							<label>
+								<p>@iis4if</p>
+							</label>
+						</div>
+					</div>
+				</a>
+				
+				
+					<a href="https://paypal.me/iis4if" target="_blank">
+				<img class="icon" src="img/paypal.png" />
+					<div>
+						<div>
+							<label>
+								<p>PayPal ادعمــني عن طريق</p>
+							</label>
+						</div>
+					</div>
+				</a>
+				
+	
+
+			</fieldset>	   
+			<label class="source">
+				<p>معلومات السورس</p>
+                <br/>
+			</label>
+			<fieldset class="source">
+				<a href="http://saiif.co/">
+					<img class="icon" src="img/CydiaIcon.png" />
+					<div>
+						<div>
+							<label>
+								<p id="source-name">
+								Saif Al-Khulaidi | سيف الخليدي									</p>
+							</label>
+						</div>
+					</div>
+				</a>
+				<div class="source-description" id="source-description">
+					<p>من افضل السورسات العربية</p>
+				</div>
+			</fieldset>
+			<footer id="footer" style="display: none;">
+				<p>
+				
+					<br />
+					<span class="source-name">Saif Al-Khulaidi</span>·
+					<span id="section">Copyright &copy; 2017-2018</span>
+									</p>
+			</footer>
+		</panel>
+		<script src="js/main.js" type="text/javascript"></script>
+		
+	</body>
+</html>
